7bb41d6de2e122061ee7e6fc50c2680e2332a2ec
Lilly Ghassemieh <lillygh@gmail.com>
2015-11-05 00:11:24 -0800

    added followers and following

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,14 @@ Rails.application.routes.draw do
 
   get '/signup' => 'users#new'
 
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :posts, only: [:create, :destroy]
+
+  resources :relationships, only: [:create, :destroy]
 
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#creating sample users with Faker gem
 User.create!(name:  "Lilly G.",
              email: "t@t.com",
              password:              "111111",
@@ -21,8 +23,17 @@ User.create!(name:  "Lilly G.",
                password_confirmation: password)
 end
 
+#creating sample posts with Faker gem
 users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.posts.create!(content: content) }
 end
+
+#following relationships
+users = User.all
+user = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed)}
+followers.each { |follower| follower.follow(user)}
